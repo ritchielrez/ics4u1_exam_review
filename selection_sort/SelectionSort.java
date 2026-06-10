d32d900bea66acae2717972f5f8501220f501855
RitchielRez <ritchielrez@gmail.com>
2026-06-10 18:43:03 -0400

    feat(selection_sort): finish SelectionSort

--- selection_sort/SelectionSort.java
+++ selection_sort/SelectionSort.java
@@ -1,10 +1,21 @@
-public class SelectionSort {
-  public static void iterativeSort(int[] nums) {
-    int i, minIndex, tmp;
-    for (i = 0; i < nums.length; i++) {
-      for (j = 0; j < nums.length; j++) {
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class SelectionSort {
+  public static void iterativeSort(ArrayList<Integer> nums) {
+    for (int i = 0; i < nums.size() - 1; i++) {
+      // Assume that the element at i is the lowest number in the set or subset.
+      int minIndex = i;
+      for (int j = i + 1; j < nums.size(); j++) {
+        // Found a number that is lower than your assumed lowest number.
+        if (nums.get(j) < nums.get(minIndex)) {
+          // Set the minIndex based on the newly found lowest number;
+          minIndex = j;
+        }
       }
+
+      // Swap the element at i with the element at minIndex.
+      Collections.swap(nums, i, minIndex);
     }
   }
 }
