b72744533cb3ebeeda1fe96e069c2784ab0b5689
Ritchiel Reza <S300145332@ddsbstudent.ca>
2026-06-10 12:11:55 -0400

    feat(binary_search): add `recursiveSearch()`

--- binary_search/BinarySearch.java
+++ binary_search/BinarySearch.java
@@ -13,4 +13,38 @@ public class BinarySearch {
 
     return -1;
   }
+
+  public static int recursiveSearch(int []nums, int match) {
+    int low = 0, high = nums.length - 1, mid;
+
+    while(low <= high) {
+      mid = (low + high) / 2;
+      if (nums[mid] > match) {
+        high = mid - 1;
+        return recursiveSearch(nums, match, low, high);
+      }
+      else if (nums[mid] < match) {
+        low = mid + 1;
+        return recursiveSearch(nums, match, low, high);
+      } else return mid;
+    }
+
+    return -1;
+  }
+
+  public static int recursiveSearch(int []nums, int match, int low, int high) {
+    while(low <= high) {
+      int mid = (low + high) / 2;
+      if (nums[mid] > match) {
+        high = mid - 1;
+        return recursiveSearch(nums, match, low, high);
+      }
+      else if (nums[mid] < match) {
+        low = mid + 1;
+        return recursiveSearch(nums, match, low, high);
+      } else return mid;
+    }
+
+    return -1;
+  }
 }
--- binary_search/Prog1.java
+++ binary_search/Prog1.java
@@ -1,10 +1,21 @@
 public class Prog1 {
   public static void main(String[] args) {
     int[] nums1 = {5, 6, 6, 9, 11, 19, 28, 32};
-    int match = 6;
+    int match = 9;
 
-    int index = BinarySearch.iterativeSearch(nums1, match);
+    int index1 = BinarySearch.iterativeSearch(nums1, match);
+    int index2 = BinarySearch.recursiveSearch(nums1, match);
 
-    System.out.println(match + " is found in nums1 at " + index);
+    if (index1 != -1) {
+      System.out.println(match + " is found (using iterative approach) in nums1 at " + index1);
+    } else {
+      System.out.println(match + " is not found (using iterative approach) in nums1");
+    }
+
+    if (index2 != -1) {
+      System.out.println(match + " is found (using recursive approach) in nums1 at " + index2);
+    } else {
+      System.out.println(match + " is not found (using recursive approach) in nums1");
+    }
   }
 }
