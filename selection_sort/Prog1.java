d0bbc96106a7d82039bb74825caf37ce8aed7afd
Ritchiel Reza <S300145332@ddsbstudent.ca>
2026-06-10 12:32:57 -0400

    feat: add selection_sort

---
+++ selection_sort/Prog1.java
@@ -0,0 +1,4 @@
+public class Prog1 {
+  public static int main(String[] args) {
+  }
+}
---
+++ selection_sort/SelectionSort.java
@@ -0,0 +1,10 @@
+public class SelectionSort {
+  public static void iterativeSort(int[] nums) {
+    int i, minIndex, tmp;
+    for (i = 0; i < nums.length; i++) {
+      for (j = 0; j < nums.length; j++) {
+
+      }
+    }
+  }
+}
